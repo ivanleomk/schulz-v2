9845c636c0fe1b7e129d2e834d2252d4b6e325eb
Ivan Leo <ivanleomk@gmail.com>
2023-05-04 18:25:45 +0800

    Adding a sign out button

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,5 +1,7 @@
 import type { NextPage } from "next";
 import DashboardTabComponent from "@/components/DashboardTabComponent";
+import { Button } from "@/components/ui/button";
+import { signOut } from "next-auth/react";
 
 const tabs = [
   {
@@ -18,7 +20,7 @@ const Dashboard: NextPage = () => {
       <div className="container flex flex-col items-start justify-between space-y-2 py-4 sm:flex-row sm:items-center sm:space-y-0 md:h-16 border-b-2">
         <h2 className="text-lg font-semibold">Schulz.AI</h2>
         <div className="ml-auto flex w-full space-x-2 sm:justify-end">
-          User Avatar
+          <Button onClick={() => signOut()}>Sign Out</Button>
         </div>
       </div>
       <div className="flex-1 space-y-4 p-8 pt-6">
